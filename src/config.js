9fe04abb01ad1aeeb6834bb7c79ff40976d05ce0
John Popo <enpower56@gmail.com>
2022-05-13 16:04:11 +0800

    first commit

---
+++ src/config.js
@@ -0,0 +1,4 @@
+import tokenizer from 'tokenizer/artifacts/contracts/erc721token.sol/Tokenizer.json';
+export const contractAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
+
+export const tokenizer = tokenizer;
